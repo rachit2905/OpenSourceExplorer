cf89428c5f997e2aca49c9a65cde7a8e30afad8d
rachit2905 <85569226+rachit2905@users.noreply.github.com>
2024-05-21 19:17:22 +0530

    improved prompt

--- app.py
+++ app.py
@@ -133,12 +133,15 @@ def get_vector_store(text_chunks, api_key):
 
 def get_conversational_chain():
     prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
-    Context:\n {context}?\n
-    Question: \n{question}\n
+    You are an intelligent assistant specializing in providing accurate and detailed answers based on the given context. Your goal is to deliver comprehensive and precise information while ensuring clarity and usefulness. If the information requested is not available in the context, simply state, "The answer is not available in the context."
 
-    Answer:
+    Context:
+    {context}
+
+    Question:
+    {question}
+
+    Detailed and accurate answer:
     """
     model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3, google_api_key=api_key)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
